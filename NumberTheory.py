e9fda68fb889775915cde415e2ee2f4044a3e99e
Manish <primespatel@gmail.com>
2022-01-03 10:35:36 +0530

    added gcd using loop function

--- NumberTheory.py
+++ NumberTheory.py
@@ -4,3 +4,10 @@ def is_odd(number):
 
 def is_even(number):
     return number%2 == 0
+
+def gcd_loop(m, n=1):
+    if n == 1:
+        return abs(m)
+    while m%n != 0:
+        m, n = n%m, m
+    return abs(n)
