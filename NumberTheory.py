821b70eaf40d0f0cc3b93896a132ece90c9b9e36
Manish <primespatel@gmail.com>
2022-01-05 23:36:55 +0530

    added gcd recursive function

--- NumberTheory.py
+++ NumberTheory.py
@@ -5,9 +5,17 @@ def is_odd(number):
 def is_even(number):
     return number%2 == 0
 
+
 def gcd_loop(m, n=1):
     if n == 1:
         return abs(m)
     while m%n != 0:
         m, n = n%m, m
     return abs(n)
+
+def gcd_recursive(m, n):
+    m, n = abs(m), abs(n)
+    if n == 0:
+        return m
+    else:
+        return gcd_recursive(n, m%n)
