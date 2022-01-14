46994c9e882fbb811dd66343c80dd206121e179a
Manish <primespatel@gmail.com>
2022-01-14 18:50:33 +0530

    tried optimizing is_prime function

--- NumberTheory.py
+++ NumberTheory.py
@@ -78,8 +78,10 @@ def euler_phi(number):
 def is_prime(number):
     if number <= 1:
         return False
+    elif number == 2:
+        return True
     else:
-        for i in range(2, int(number**0.5)+1):
+        for i in range(3, int(number**0.5)+1, 1):
             if number%i == 0:
                 return False
         return True
